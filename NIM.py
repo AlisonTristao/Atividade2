de777cf5dad40a071745a5fb309d144850e1daa3
Maria Eduarda <maria.justus.luz@gmail.com>
2023-02-06 20:50:32 -0300

    jogo nim

---
+++ NIM.py
@@ -0,0 +1,19 @@
+def computador_escolhe_jogada(n,m):
+    if n % m:
+        
+def partida():
+    n = int(input("Quantas peças? "))
+    m = int(input("Limite de peças por jogada? "))
+    if n % m == 0:
+            print()
+            print("Computador começa!")
+            print()
+    else:
+            print()
+            print("Voce começa!)
+            print()
+            while n != 0:
+                computador_escolhe_jogada(n,m)
+                usuario_escolhe_jogada(n,m)
+            if n == 0
+                return("Fim do jogo! O computador ganhou!")
